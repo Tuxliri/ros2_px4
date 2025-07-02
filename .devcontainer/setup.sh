959083c578291acadb16109d8cc2b38aa1d70f3d
Davide Iafrate <dvde.iafrate98@gmail.com>
2025-07-02 09:10:45 +0000

    Disable download of unused px4 ros2 repos

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,7 +3,7 @@ set -e
 
 cd /home/developer/ros2_ws
 
-vcs import --recursive < /home/developer/ros2_ws/src/px4.repos src
+# vcs import --recursive < /home/developer/ros2_ws/src/px4.repos src
 
 rosdep update --rosdistro $ROS_DISTRO
 rosdep install -i --from-path src --rosdistro $ROS_DISTRO -y
